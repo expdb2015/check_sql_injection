d890346f5e2792b006405fe2b34a1da2a1dd1837
liuyuesheng <620796236@qq.com>
2012-09-20 15:14:09 +0800

    test files

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+$_GET["id"];
+$a = $_GET["id"];
+$a = $_GET["id"];
+$a = $_GET["id"];
+$a = $_GET["id"];
+$ids = $_GET["id"];   //测试而已
+$ids = $_GET["id"];   //测试而已
+
+$ids = intval($_GET["id"]);
+$ids = $_GET["id"];   //测试而已
+
+$result=$db->query("SELECT * FROM `xcar_topic_users_jilu` WHERE `topicid` =8 and `id` ='".$id."' and `ids` = {$ids}  ")
